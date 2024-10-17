893a3818eb52be3b251e54b509f27ededee2b85e
Yahya Tirsi <142328845+Yahya-tirsi@users.noreply.github.com>
2024-10-17 18:08:16 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+// import CVForm from './components/creer-cv';
+// import UserProfileForm from './components/creer-cv';
+import Cvdeux from './components/cv2';
+
+function App() {
+  return (
+    <div className="App">
+      <Cvdeux />
+    </div>
+  );
+}
+
+export default App;
